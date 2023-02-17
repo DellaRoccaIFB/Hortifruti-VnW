3d7283ce50ffc8c9016728c6be2acf6f71965129
Joao Pedro Della Rocca <joao.camargos@estudante.ifb.edu.br>
2023-02-16 23:51:15 -0300

    iniciado o desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Alface from "./img/alface.png";
+import Laranja from "./img/laranja.png";
+import Cereja from "./img/cereja.png";
+import Cenoura from "./img/cenoura.png";
+import Manga from "./img/manga.png";
+import Limão from "./img/limao.png";
+import Beterraba from "./img/beterraba.png";
+import Tomate from "./img/tomate.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    NomeLoja: "HORTIFRUTI",
+    frutas: [
+      {
+        nomeFruta: "Alface",
+        imagemFruta: `${Alface}`,
+      },
+      {
+        nomeFruta: "Laranja",
+        imagemFruta: `${Laranja}`,
+      },
+      {
+        nomeFruta: "Cereja",
+        imagemFruta: `${Cereja}`,
+      },
+      {
+        nomeFruta: "Cenoura",
+        imagemFruta: `${Cenoura}`,
+      },
+      {
+        nomeFruta: "Manga",
+        imagemFruta: `${Manga}`,
+      },
+      {
+        nomeFruta: "Limão",
+        imagemFruta: `${Limão}`,
+      },
+      {
+        nomeFruta: "Beterraba",
+        imagemFruta: `${Beterraba}`,
+      },
+      {
+        nomeFruta: "Tomate",
+        imagemFruta: `${Tomate}`,
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <body>
+        <section class="mainSection">
+          <h1 class="NomeLoja">{this.state.NomeLoja}</h1>
+          <div class="ContainerFrutas">
+            {this.state.frutas.map((item) => (
+              <>
+                <img src={item.imagemFruta} />
+              </>
+            ))}
+            <div class="Carrinho"></div>
+          </div>
+        </section>
+      </body>
+    );
+  }
 }
 
 export default App;
