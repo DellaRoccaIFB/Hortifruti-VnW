f83f6023db3b53a2c9e16b388bd9c44961b25358
Joao Pedro Della Rocca <joao.camargos@estudante.ifb.edu.br>
2023-02-27 19:56:40 -0300

    terminei o desafio

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import Manga from "./img/manga.png";
 import Limão from "./img/limao.png";
 import Beterraba from "./img/beterraba.png";
 import Tomate from "./img/tomate.png";
+import Carrinho from "./img/ShoppingCart.png"
 
 class App extends Component {
   state = {
     NomeLoja: "HORTIFRUTI",
+    SubtituloLoja: "Nossos Produtos",
     frutas: [
       {
         nomeFruta: "Alface",
@@ -46,6 +48,7 @@ class App extends Component {
         imagemFruta: `${Tomate}`,
       },
     ],
+    ImagemCarrinho: `${Carrinho}`,
   };
 
   render() {
@@ -53,13 +56,28 @@ class App extends Component {
       <body>
         <section class="mainSection">
           <h1 class="NomeLoja">{this.state.NomeLoja}</h1>
-          <div class="ContainerFrutas">
-            {this.state.frutas.map((item) => (
-              <>
-                <img src={item.imagemFruta} />
-              </>
-            ))}
-            <div class="Carrinho"></div>
+          <h3 class="VnW">VnW</h3>
+          <h3 class="SubtituloLoja">{this.state.SubtituloLoja}</h3>
+          <div class="FrutasECarrinho">
+            <section class="ContainerFrutas">
+              <div class="BoxFrutas">
+                {this.state.frutas.map((item) => (
+                  <>
+                    <img class="imagemFruta" src={item.imagemFruta} />
+                  </>
+                ))}
+              </div>
+            </section>
+            <div class="Carrinho">
+              <div class="RetanguloArredondado">
+                <img src={this.state.ImagemCarrinho} alt="ImagemCarrinho"/>
+                <p class="DescricaoCarrinho">Arraste o produto aqui para colocar no carrinho</p>
+              </div>
+              <div class="PlusAndMinusButton">
+                <button class="PlusButton"><div class="line1"></div><div class="line2"></div></button>
+                <button class="MinusButton"><div class="line3"></div></button>
+              </div>
+            </div>
           </div>
         </section>
       </body>
